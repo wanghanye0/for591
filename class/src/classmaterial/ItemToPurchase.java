8b784ebaab2a2f02cca9a80d0cb232f154463828
wanghanye0 <53504038+wanghanye0@users.noreply.github.com>
2020-10-15 16:20:55 -0400

    prompt the user for two items about items' name, price and quantity

--- class/src/classmaterial/ItemToPurchase.java
+++ class/src/classmaterial/ItemToPurchase.java
@@ -1,5 +1,7 @@
 package classmaterial;
 
+import java.util.Scanner;
+
 public class ItemToPurchase {
 	
 	String itemName;
@@ -29,6 +31,28 @@ public class ItemToPurchase {
 	}
 	
 	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		ItemToPurchase itp1 = new ItemToPurchase();
+		System.out.println("Item 1");
+		System.out.println("Enter the item name:");
+		itp1.itemName = s.nextLine();
+		System.out.println("Enter the item price:");
+		itp1.itemPrice = s.nextInt();
+		System.out.println("Enter the item quantity:");
+		itp1.itemQuantity = s.nextInt();
+		
+		ItemToPurchase itp2 = new ItemToPurchase();
+		System.out.println("Item 2");
+		System.out.println("Enter the item name:");
+		itp2.itemName = s.nextLine();
+		System.out.println("Enter the item price:");
+		itp2.itemPrice = s.nextInt();
+		System.out.println("Enter the item quantity:");
+		itp2.itemQuantity = s.nextInt();
+		
+		
+	}
 	
 	
 }
