047c44c3a4c783497be4930881f9e0bdd1dc1231
wanghanye0 <53504038+wanghanye0@users.noreply.github.com>
2020-10-15 13:16:22 -0400

    added all var and setters and getters

--- class/src/classmaterial/ItemToPurchase.java
+++ class/src/classmaterial/ItemToPurchase.java
@@ -5,6 +5,25 @@ public class ItemToPurchase {
 	String itemName;
 	int itemPrice;
 	int itemQuantity;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+	
 	
 	
 	
